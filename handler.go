873647427c32710ae0de41d9e61e459ae02ae22d
SylvanusChime <chimesylvanus.chi@gmail.com>
2026-09-12 19:50:24 -0600

    modified Metric handler

--- handler.go
+++ handler.go
@@ -25,19 +25,19 @@
 // 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 // }
 
-// func MetricsHandler(store Store) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "text/plain; version=0.0.4")
-// 		total, done := store.Stats()
-// 		// Prometheus exposition format
-// 		w.Write([]byte("# HELP task_api_tasks_total Total number of tasks.\n"))
-// 		w.Write([]byte("# TYPE task_api_tasks_total gauge\n"))
-// 		w.Write([]byte("task_api_tasks_total " + strconv.Itoa(total) + "\n"))
-// 		w.Write([]byte("# HELP task_api_tasks_done Number of completed tasks.\n"))
-// 		w.Write([]byte("# TYPE task_api_tasks_done gauge\n"))
-// 		w.Write([]byte("task_api_tasks_done " + strconv.Itoa(done) + "\n"))
-// 	}
-// }
+func MetricsHandler(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; version=0.0.4")
+		total, done := store.Stats()
+		// Prometheus exposition format
+		w.Write([]byte("# HELP task_api_tasks_total Total number of tasks.\n"))
+		w.Write([]byte("# TYPE task_api_tasks_total gauge\n"))
+		w.Write([]byte("task_api_tasks_total " + strconv.Itoa(total) + "\n"))
+		w.Write([]byte("# HELP task_api_tasks_done Number of completed tasks.\n"))
+		w.Write([]byte("# TYPE task_api_tasks_done gauge\n"))
+		w.Write([]byte("task_api_tasks_done " + strconv.Itoa(done) + "\n"))
+	}
+}
 
 // func ListTasksHandler(store Store) http.HandlerFunc {
 // 	return func(w http.ResponseWriter, r *http.Request) {
